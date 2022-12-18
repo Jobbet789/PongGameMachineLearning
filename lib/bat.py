16111e82a4a0aa1188ba2ea58dacc945519c9af2
Jobbet789 <77113631+Jobbet789@users.noreply.github.com>
2022-12-18 14:39:27 +0100

    changed something

--- lib/bat.py
+++ lib/bat.py
@@ -1,33 +1,32 @@
 import pygame
 
-class Bat:
-	WIDTH = 20
-	HEIGHT = 80
-	def __init__(self, WINHEIGHT, side):
-		# random velocity, change this later
-		self.vel = 0
-		self.speed = 7
-		self.direction = 0
+class Bat: # Class for the bat
+	WIDTH = 20 # the width of the bat
+	HEIGHT = 80 # the height of the bat
+	def __init__(self, WINHEIGHT, side): # initialize the bat
+		self.vel = 0 # the real velocity
+		self.speed = 7 # the velocity when moving
+		self.direction = 0 # the direction of the bat (1, -1 or 0)
 
-		self.side = side
+		self.side = side # the side of the bat (left or right)
 
-		self.WINHEIGHT = WINHEIGHT
+		self.WINHEIGHT = WINHEIGHT # the height of the window
 
-		self.score = 0
+		self.score = 0 # the score of the abd
 
-	def init(self, startX, startY):
-		self.x = startX
-		self.y = startY
+	def init(self, startX, startY): # initialize the bat
+		self.x = startX # the x position of the bat
+		self.y = startY # the y position of the bat
 
-	def move(self):
-		self.vel = self.speed*self.direction
+	def move(self): # move the bat
+		self.vel = self.speed*self.direction # calculate the velocity
 
-		self.y += self.vel
+		self.y += self.vel # move the bat
 
-		self.y = max(0, min(self.y, self.WINHEIGHT - self.HEIGHT))
+		self.y = max(0, min(self.y, self.WINHEIGHT - self.HEIGHT)) # maximize and minimize the y position of the bat
 
-	def draw(self, win):
-		pygame.draw.rect(win, (255, 255, 255), pygame.Rect(self.x, self.y, self.WIDTH, self.HEIGHT))
+	def draw(self, win): # draw the bat
+		pygame.draw.rect(win, (255, 255, 255), pygame.Rect(self.x, self.y, self.WIDTH, self.HEIGHT)) # draw the bat
 
 
 
